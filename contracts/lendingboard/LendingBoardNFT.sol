b35b2d9b3e1bb47fcb0fe29d401fc88b050fb74a
gongjuheon <yh6543210@gmail.com>
2023-07-13 16:56:26 +0900

    Mapping & Metadata Getter & Setter Implementation (2023.07.13)

--- contracts/lendingboard/LendingBoardNFT.sol
+++ contracts/lendingboard/LendingBoardNFT.sol
@@ -5,17 +5,17 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 import "hardhat/console.sol";
 
-contract LendingBoardNFT is ERC721URIStorage, Ownable {
+contract LendingBoardNFT is ERC721URIStorage, ERC721Enumerable, Ownable {
+    
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-
-    // mapping for NFT TokenID -> metadata structure
-    mapping(uint256 => LendingBoardNFT.metadata) public nftMetadata;
-
-    struct metadata {
+    
+    struct Metadata {
+        uint256 proposalId;
         address borrower;
         uint256 amount;
         uint256 dueDate;
@@ -24,19 +24,38 @@ contract LendingBoardNFT is ERC721URIStorage, Ownable {
         uint256 paybackAmount;
     }
 
+    // mapping for NFT TokenID -> metadata structure
+    mapping(uint256 => Metadata) public mappedNFT;
+
     constructor() ERC721("NFT Bond", "NFTB") {}
 
     function mintNFT(
-        address _recipient
+        address _recipient,
+        uint256 _proposalId, 
+        address _borrower, 
+        uint256 _amount,
+        uint256 _dueDate,
+        uint256 _contractTimestamp,
+        uint256 _interestRate,
+        uint256 _paybackAmount
     )
         public
         returns (uint256)
     {
         _tokenIds.increment();
-
         uint256 newItemId = _tokenIds.current();
+        
         _mint(_recipient, newItemId);
-        // _setTokenURI(newItemId, _metadata);
+        setMetadataAndmappingNFTWithMetadata(
+            newItemId, 
+            _proposalId, 
+            _borrower, 
+            _amount, 
+            _dueDate, 
+            _contractTimestamp, 
+            _interestRate, 
+            _paybackAmount
+        );
 
         return newItemId;
     }
@@ -45,7 +64,8 @@ contract LendingBoardNFT is ERC721URIStorage, Ownable {
         _burn(tokenId);
     }
 
-    function setNFTmetadatas(
+    function setMetadataAndmappingNFTWithMetadata(
+        uint256 _tokenId,
         uint256 _proposalId, 
         address _borrower, 
         uint256 _amount,
@@ -57,7 +77,8 @@ contract LendingBoardNFT is ERC721URIStorage, Ownable {
         public 
     {
         // add new mapping
-        nftMetadata[_proposalId] = metadata(
+        mappedNFT[_tokenId] = Metadata(
+            _proposalId,
             _borrower, 
             _amount, 
             _dueDate, 
@@ -67,7 +88,44 @@ contract LendingBoardNFT is ERC721URIStorage, Ownable {
         );
     }
 
-    function getNFTmetadatas(uint256 _proposalId) public view returns (metadata memory) {
-        return nftMetadata[_proposalId - 1];
+    function getNFTmetadata(uint256 _tokenId) public view returns (Metadata memory) {
+        return mappedNFT[_tokenId];
+    }
+
+    // Overriding ERC721Enumerable
+    function _burn(uint256 tokenId) internal virtual override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721URIStorage, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _beforeTokenTransfer( // have to be modified
+        address from, 
+        address to, 
+        uint256 firstTokenId, 
+        uint256 batchSize
+    ) internal virtual override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, firstTokenId, batchSize);
+    }
+
+    function _afterTokenTransfer( // have to be modified
+        address from, 
+        address to, 
+        uint256 firstTokenId, 
+        uint256 batchSize
+    ) internal virtual override {
+        super._afterTokenTransfer(from, to, firstTokenId, batchSize);
     }
 }
+
+/**
+  * Updates
+ */
+// 2023.07.13
+// - ERC721Enumerable 을 사용하기 위하여 Overriding
