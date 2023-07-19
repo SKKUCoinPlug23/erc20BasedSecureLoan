184f071b420133b327f34eef95f089ce1b676038
gongjuheon <yh6543210@gmail.com>
2023-07-19 17:52:12 +0900

    Error Resolved & loggingEnabled

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,12 +2,20 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      // gas: 12000000,
+      loggingEnabled: true,
+      allowUnlimitedContractSize: true,
+    },
+  },
   solidity: {
     version: "0.8.13",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
+        runs: 500
       },
       viaIR :true,
     }
