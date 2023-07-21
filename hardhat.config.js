ae96c80fa17a88632ebe2d714fca5aef81bf8e72
서준 <seojune@seojun-ui-MacBookPro.local>
2023-07-21 12:17:48 +0900

    Sepolia test network deployment basic settinng

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -8,6 +12,10 @@ module.exports = {
       loggingEnabled: true,
       allowUnlimitedContractSize: true,
     },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
   },
   solidity: {
     version: "0.8.13",
